1a5a597ab755d285a37f5d85ad8e41b6f6246e48
Vishal Nirmal <vishalnirmal53@gmail.com>
2021-01-26 15:39:55 +0530

    Added styling to the page for mobile version

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var share = document.querySelector(".share");
 share.addEventListener("click", ()=>{
     var share_bubble = document.querySelector(".share-bubble");
-    if(share_bubble.classList.contains("active"))
-        share_bubble.classList.remove("active");
+    if(share_bubble.parentElement.classList.contains("active"))
+        share_bubble.parentElement.classList.remove("active");
     else
-        share_bubble.classList.add("active");
+        share_bubble.parentElement.classList.add("active");
 });
